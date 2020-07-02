8d0dec5ba5c1031b4c0ddcf4e4859ce64d87bb4c
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-02 04:24:35 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,6 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
-  array.count {|array| array.to.s.include? }
+  array.count {|array| array.s.include? }
 end
 
 def count_empty_strings(array)
