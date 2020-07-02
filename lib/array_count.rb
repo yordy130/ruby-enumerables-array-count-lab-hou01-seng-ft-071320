7603d21b923be63cec919832170f7ae7eff9c70f
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-02 04:49:46 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,13 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  array.count do |x|
+    x.class == String
+  end
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array.count do |c|
+    c == ""
+  end
 end
