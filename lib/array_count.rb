dc5f46118ab34ab53cedaaf87300351e8757762d
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-02 04:47:33 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -8,6 +8,6 @@ end
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
   array.count do |c|
-    c.class == array
+    c == array
   end
 end
